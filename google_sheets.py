2a8413fb45b090abd50300e66d73ddac99568683
Tu Nombre <tuemail@ejemplo.com>
2026-01-28 01:22:23 +0100

    a

--- google_sheets.py
+++ google_sheets.py
@@ -7,22 +7,18 @@ SCOPE = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/au
 CREDS = ServiceAccountCredentials.from_json_keyfile_name("credenciales.json", SCOPE)
 CLIENT = gspread.authorize(CREDS)
 
-
 SHEET = CLIENT.open("Registros_DB").sheet1
 
-def guardar_lead(nombre, telefono, direccion, notas):
-
+def guardar_lead(nombre, apellidos, telefono, direccion, notas):
     fecha_hora = datetime.now().strftime("%d/%m/%Y %H:%M")
-
-    SHEET.append_row([fecha_hora, nombre, telefono, direccion, notas])
+    SHEET.append_row([fecha_hora, nombre, apellidos, telefono, direccion, notas])
     return True
 
 def obtener_todos_registros():
-
     data = SHEET.get_all_records()
     return pd.DataFrame(data)
 
 def actualizar_todo(dataframe):
-
+    df_limpio = dataframe.fillna("")
     SHEET.clear()
-    SHEET.update([dataframe.columns.values.tolist()] + dataframe.values.tolist())
+    SHEET.update([df_limpio.columns.values.tolist()] + df_limpio.values.tolist())
