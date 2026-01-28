cacf1ea648e09c98029695055c9772706f3f3c94
Tu Nombre <tuemail@ejemplo.com>
2026-01-28 01:59:50 +0100

    a

--- main.py
+++ main.py
@@ -3,11 +3,19 @@ from twilio.twiml.messaging_response import MessagingResponse
 from openai import OpenAI
 import os
 import google_sheets as db 
+import streamlit as st
 
 app = Flask(__name__)
+
+try:
+    clave_groq = st.secrets["GROQ_API_KEY"]
+except FileNotFoundError:
+    print("ERROR: No encuentro el archivo .streamlit/secrets.toml")
+    clave_groq = "ERROR"
+
 client = OpenAI(
     base_url="https://api.groq.com/openai/v1",
-    api_key="GROQ_API_KEY"
+    api_key=clave_groq
 )
 
 conversation_history = {}
@@ -27,29 +35,28 @@ def whatsapp_reply():
     incoming_msg = request.values.get('Body', '').strip()
     sender_id = request.values.get('From', '')
 
-
     if sender_id not in conversation_history:
         conversation_history[sender_id] = [{"role": "system", "content": SYSTEM_PROMPT}]
     
     conversation_history[sender_id].append({"role": "user", "content": incoming_msg})
 
-    response = client.chat.completions.create(
-        model="llama-3.3-70b-versatile",
-        messages=conversation_history[sender_id],
-        temperature=0.7
-    )
-    
-    bot_reply = response.choices[0].message.content
+    try:
+        response = client.chat.completions.create(
+            model="llama-3.3-70b-versatile",
+            messages=conversation_history[sender_id],
+            temperature=0.7
+        )
+        bot_reply = response.choices[0].message.content
+    except Exception as e:
+        return str(MessagingResponse().message(f"Error en IA: {e}"))
 
-    
     resp_twilio = MessagingResponse()
     
     if '"ACTION": "SAVE"' in bot_reply:
-        
-        db.guardar_lead("Usuario WhatsApp", "", sender_id, "Dirección capturada en chat", incoming_msg)
-        
-        msg = resp_twilio.message("¡Gracias! Hemos guardado tus datos correctamente.")
-        del conversation_history[sender_id]
+        db.guardar_lead("Usuario WhatsApp", "", sender_id, "Direccion capturada", incoming_msg)
+        msg = resp_twilio.message("Gracias. Hemos guardado tus datos correctamente.")
+        if sender_id in conversation_history:
+            del conversation_history[sender_id]
     else:
         conversation_history[sender_id].append({"role": "assistant", "content": bot_reply})
         msg = resp_twilio.message(bot_reply)
